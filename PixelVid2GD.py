28d6deaa66bdd761a2f57ad4c4f8c927e5cfbf84
HenrySck075 <email.is.not.vaild9@gmail.com>
2023-01-29 16:02:39 +0700

    d

--- PixelVid2GD.py
+++ PixelVid2GD.py
@@ -5,8 +5,8 @@ from PIL import Image
 SAVE_FILE_PATH = os.path.join(os.getenv('LocalAppData'), 'GeometryDash')
 data={
     "ham": "<k>k_0</k><d><k>kCEK</k><i>4</i><k>k1</k><i>0</i><k>k2</k><s>[[NAME]]</s><k>k4</k><s>",
-    "bur": "kS38,1_0_2_0_3_0_11_0_12_0_13_0_4_-1_6_1000_7_1_15_1_18_0_8_1|1_0_2_0_3_0_11_255_12_255_13_255_4_-1_6_1001_7_1_15_1_18_0_8_1|1_0_2_0_3_0_11_255_12_255_13_255_4_-1_6_1009_7_1_15_1_18_0_8_1|1_255_2_255_3_255_11_255_12_255_13_255_4_-1_6_1002_5_1_7_1_15_1_18_0_8_1|1_135_2_135_3_135_11_255_12_255_13_255_4_-1_6_1005_5_1_7_1_15_1_18_0_8_1|1_255_2_125_3_0_11_255_12_255_13_255_4_-1_6_1006_5_1_7_1_15_1_18_0_8_1|1_255_2_0_3_0_11_255_12_255_13_255_4_-1_6_10_7_1_15_1_18_0_8_1|,kA1,[[SONG_ID]],kA13,0,kA15,0,kA16,0,kA14,,kA6,13,kA7,0,kA17,0,kA18,0,kS39,0,kA2,0,kA3,0,kA8,0,kA4,4,kA9,0,kA10,0,kA11,0;",
-    "ger": "</s><k>k3</k><s>[[DESC]]</s><k>k14</k><t /><k>k46</k><i>0</i><k>k5</k><s></s><k>k13</k><t /><k>k21</k><i>2</i><k>k48</k><i>69</i><k>k16</k><i>1</i><k>k23</k><s>3</s><k>k8</k><i>0</i><k>k45</k><i>0</i><k>k80</k><i>0</i><k>k50</k><i>0</i><k>k47</k><t /><k>k84</k><i>0</i><k>kI1</k><r>0</r><k>kI2</k><r>0</r><k>kI3</k><r>0</r><k>kI5</k><i>6</i><k>kI6</k><d><k>0</k><s>0</s><k>1</k><s>0</s><k>2</k><s>0</s><k>3</k><s>0</s><k>4</k><s>0</s><k>5</k><s>0</s><k>6</k><s>0</s><k>7</k><s>0</s><k>8</k><s>0</s><k>9</k><s>0</s><k>10</k><s>0</s><k>11</k><s>0</s><k>12</k><s>0</s></d></d>"
+    "bur": "kS38,1_0_2_0_3_0_11_0_12_0_13_0_4_-1_6_1000_7_1_15_1_18_0_8_1|1_0_2_0_3_0_11_255_12_255_13_255_4_-1_6_1001_7_1_15_1_18_0_8_1|1_0_2_0_3_0_11_255_12_255_13_255_4_-1_6_1009_7_1_15_1_18_0_8_1|1_255_2_255_3_255_11_255_12_255_13_255_4_-1_6_1002_5_1_7_1_15_1_18_0_8_1|1_135_2_135_3_135_11_255_12_255_13_255_4_-1_6_1005_5_1_7_1_15_1_18_0_8_1|1_255_2_125_3_0_11_255_12_255_13_255_4_-1_6_1006_5_1_7_1_15_1_18_0_8_1|1_255_2_0_3_0_11_255_12_255_13_255_4_-1_6_10_7_1_15_1_18_0_8_1|,kA13,0,kA15,0,kA16,0,kA14,,kA6,13,kA7,0,kA17,0,kA18,0,kS39,0,kA2,0,kA3,0,kA8,0,kA4,4,kA9,0,kA10,0,kA11,0;",
+    "ger": "</s><k>k3</k><s>[[DESC]]</s><k>k14</k><t /><k>k46</k><i>0</i><k>k5</k><s></s><k>k13</k><t /><k>k21</k><i>2</i><k>k48</k><i>69</i><k>k16</k><i>1</i><k>k23</k><s>3</s><k>k8</k><i>0</i><k>k45</k><i>0</i><k>k80</k><i>0</i><k>k50</k><i>0</i><k>k47</k><t /><k>k84</k><i>0</i><k>kI1</k><r>[[EDITORX]]</r><k>kI2</k><r>0</r><k>kI3</k><r>0</r><k>kI5</k><i>6</i><k>kI6</k><d><k>0</k><s>0</s><k>1</k><s>0</s><k>2</k><s>0</s><k>3</k><s>0</s><k>4</k><s>0</s><k>5</k><s>0</s><k>6</k><s>0</s><k>7</k><s>0</s><k>8</k><s>0</s><k>9</k><s>0</s><k>10</k><s>0</s><k>11</k><s>0</s><k>12</k><s>0</s><k>k45</k><i>[[SONG_ID]]</i><k>k64</k><i>6</i></d></d>"
 }
 def xor_bytes(data: bytes, value: int) -> bytes:
     return bytes(map(lambda x: x ^ value, data))
@@ -38,7 +38,7 @@ def replacenth(string, sub, wanted, n):
     after = string[where:]
     after = after.replace(sub, wanted, 1)
     newString = before + after
-    print(newString)
+    return newString
 #print(decrypt(re.search("<k>k4</k><s>(.*)</s>",decrypted).group(1),False))
 levle_array=[
     "1,1612,2,-29,3,705;",
@@ -77,7 +77,7 @@ def readFrames():
 
             pil_im = Image.fromarray(converted)
 
-            im = pil_im.resize((math.floor(width/(height/40)),40))
+            im = pil_im.resize((math.floor((width/height)*40),40))
                     
             pxArray=[]
             #please help me
@@ -107,13 +107,13 @@ def readFrames():
                     for hi,h in enumerate(w):
                         if pxArray[wi][hi] != last_pxArray[wi][hi]:
                             levle_array.append(f"1,1006,2,{trig_x},3,{trig_y+yoffs},10,99999999999,50,10,49,{'{0}a{1}a{2}a1a1'.format(*rgb_to_hsv(pxArray[wi][hi]))},51,{groupIds[wi][hi]},52,1,48,1;") #pulse trigger
-                            yoffs+=2
+                            yoffs+=4
                             changes+=1
             last_pxArray=deepcopy(pxArray)
         elif not ret:
             break
         if frames==0: print("First frame, nothing to compare")
-        else: pass#print(f"{frames} with {frames-1}: {changes} change(s)      ",end="\r")
+        else: print(f"{frames} with {frames-1}: {changes} change(s)      ",end="\r")
         trig_x+=19.2
         cv2.waitKey(int(1000/30))
         frames+=1
@@ -133,7 +133,7 @@ for i in re.finditer("<k>k_(\d+)</k><d><k>kCEK</k>", saveData[1]):
     saveData[1] = replacenth(saveData[1], i.group(), f"<k>k_{int(i.group(1))+1}</k><d><k>kCEK</k>", s)
     s=2
 saveData:str = ''.join([saveData[0], "<k>_isArr</k><t />", data['ham'], data['bur'], levle_string, data['ger'], saveData[1]])
-saveData = saveData.replace("[[NAME]]", re.sub("[^a-z|0-9]","",videoFile.replace(".mp4","").split(".")[0][0:30])).replace("[[DESC]]", "").replace("[[SONG_ID]]",songID)
+saveData = saveData.replace("[[NAME]]", re.sub("[^a-z|0-9]","",videoFile.replace(".mp4","").split(".")[0][0:30])).replace("[[DESC]]", "").replace("[[SONG_ID]]",songID).replace("[[EDITORX]]",trig_x+3000)
 with open(os.path.join(SAVE_FILE_PATH,"CCLocalLevels.dat"),"w") as w:
     w.write(saveData)
 
